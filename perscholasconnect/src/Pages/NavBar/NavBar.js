f29ff0398c07df0188169c9e51b8bd5d799271c6
Stacey <stacey.l.joseph@gmail.com>
2025-07-12 17:49:50 -0400

    Added NavBar

---
+++ perscholasconnect/src/Pages/NavBar/NavBar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import toryProfile from "../../assets/images/staffprofile.jpg";
+import "./styles.css";
+
+function NavBar() {
+  return (
+    <div className="App">
+      <nav className="Navbar">
+        <div className="MainNav">
+          <div className="Nav-header">
+            <h1>Per Scholas Connect</h1>
+          </div>
+
+          <div className="Nav-Links">
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/contact">About</a>
+              </li>
+              <li>
+                <a href="/contact">Alumni</a>
+              </li>
+              <li>
+                <a href="/about">Mentorship</a>
+              </li>
+              <li>
+                <a href="/contact">New Learners</a>
+              </li>
+              <li>
+                <a href="/contact">Events</a>
+              </li>
+
+              <li>
+                <a href="/contact">My Profile</a>
+              </li>
+            </ul>
+          </div>
+          <div className="Profile">
+            <img src={toryProfile} alt="Profile" className="ProfileImage" />
+          </div>
+          <button className="LogOutButton">Logout</button>
+        </div>
+      </nav>
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default NavBar;
